adcb5b1aa9bc6627de1441c3ddfd41fb1d90f544
torluca <luca.t90@gmail.com>
2023-02-06 17:26:29 +0100

    adding files for SFH library and stellar mass function library

---
+++ src/rail_lib_gp_comp/rail_lib_gp_comp.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+# Copyright (C) 2022 University Observatory, Ludwig-Maximilians-Universitaet Muenchen
+# Author: Luca Tortorelli
+
+# System imports
+from __future__ import (print_function, division, absolute_import,
+                        unicode_literals)
+
+# External modules
+
+# rail_lib_gp_comp imports
+
+
+def check_implemented_components(input_component):
+    """
+
+    Parameters
+    ----------
+    input_component
+
+    Returns
+    -------
+
+    """
+    implemented_modelling_components = ['Schechter']
+
+    if input_component in implemented_modelling_components:
+        return True
+    else:
+        return False
---
+++ src/rail_lib_gp_comp/star_formation_histories_library.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+# Copyright (C) 2022 University Observatory, Ludwig-Maximilians-Universitaet Muenchen
+# Author: Luca Tortorelli
+
+# System imports
+from __future__ import (print_function, division, absolute_import,
+                        unicode_literals)
+
+# External modules
+from abc import ABC, abstractmethod
+
+# rail_lib_gp_comp imports
